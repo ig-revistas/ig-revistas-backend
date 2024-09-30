0cfb3fb7b67eed925342c8e5d3c67bef2df60662
AugustoKopach <akopach05@gmail.com>
2024-09-30 17:16:49 -0300

    Primer commit

---
+++ Trabajo/src/test/java/Revistas/Trabajo/TrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package Revistas.Trabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
